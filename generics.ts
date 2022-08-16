b8d88d96eb29ddea606a8cd843ccf766dfbc7888
Anna Egorova <annaegorova9791@gmail.com>
2022-08-16 19:19:46 +0300

    create function

--- generics.ts
+++ generics.ts
@@ -15,3 +15,13 @@ interface SuperUser{
 
 type Default = Person<User>
 type Admin = Person<SuperUser>
+
+
+function getRandom<T>(items: T[]): T {
+    const randomIndex = ~~(Math.random()*items.length)
+    return items[randomIndex]
+}
+
+const el1 = getRandom([1,2,3,4])
+const el2 = getRandom(["1","2",3,4])
+console.log(typeof el2)
