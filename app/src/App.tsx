b55dcb6fb73ce784b524258b0386cfebdb6795cc
Anna Egorova <annaegorova9791@gmail.com>
2022-08-18 16:39:40 +0300

    add redirection

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Nav } from "./components/Outline";
 import { Home } from "./components/Home";
 import { Test } from "./components/Test";
@@ -15,6 +15,7 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path="test" element={<Test/>}/>
         <Route path="about" element={<About/>}/>
+        <Route path="about-us" element={<Navigate to="/about" replace />}/>
         <Route path="*" element={<ErrorPage/>}/>
 
       </Route>
